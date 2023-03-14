d50e3c54285520257fcaef615374088926812960
Kardast <dottorsf@gmail.com>
2023-03-14 11:26:07 +0100

    Working on front-end (printing orders)

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -8,14 +8,8 @@ import { Order, OrderClient } from './api.service';
 })
 export class AppComponent {
   title = 'order-scheduling-angular';
-  array = ["1", "2", "3"];
   list: Order[] = [];
   constructor(private orderClient:OrderClient){
     orderClient.list().subscribe(list=>this.list = list);
-  }
-
-  changeTitle(){
-    this.title = "Titolo 2";
-    this.array.push("4");
   }
 }
