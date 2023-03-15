725e17405f9346fc453b6b00757a1546d78f828d
Kardast <dottorsf@gmail.com>
2023-03-15 16:56:28 +0100

    backend files

--- back-end/CodeNotion.Academy.OrderScheduling/Cqrs/Queries/GetListQuery.cs
+++ back-end/CodeNotion.Academy.OrderScheduling/Cqrs/Queries/GetListQuery.cs
@@ -19,7 +19,16 @@ internal class GetListHandler : IRequestHandler<GetListQuery, List<Order>>
     public async Task<List<Order>> Handle(GetListQuery request, CancellationToken cancellationToken)
     {
         var orders = _db.Orders;
-        if (request.Customer != null)
+
+        if (request is { Customer: { }, OrderNumber: { } })
+        {
+            return await orders.Where(order =>
+                    request.Customer != null &&
+                    order.Customer.ToLower().Contains(request.Customer.ToLower()) &&
+                    order.OrderNumber.ToLower().Contains(request.OrderNumber.ToLower()))
+                .ToListAsync(cancellationToken: cancellationToken);
+        }
+        else if (request.Customer != null)
         {
             return await orders.Where(order => order.Customer.ToLower().Contains(request.Customer.ToLower()))
                 .ToListAsync(cancellationToken: cancellationToken);
