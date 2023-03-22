eaa0173d6dd46b3d0fb50716ac0dd83d02825da5
Dario <dario.ferrai>
2023-03-22 16:27:33 +0200

    Order tests

--- back-end/CodeNotion.Academy.OrderScheduling.Tests/OrderTests.cs
+++ back-end/CodeNotion.Academy.OrderScheduling.Tests/OrderTests.cs
@@ -2,34 +2,93 @@ using CodeNotion.Academy.OrderScheduling.Cqrs.Commands;
 using CodeNotion.Academy.OrderScheduling.Database;
 using CodeNotion.Academy.OrderScheduling.Models;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace CodeNotion.Academy.OrderScheduling.Tests;
 
 public class OrderTests
 {
+    private readonly IMediator _mediator;
+    private readonly IServiceProvider _provider;
+
+    public OrderTests()
+    {
+        var services = new ServiceCollection();
+        services.AddDbContext<DatabaseContext>();
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
+        _provider = services.BuildServiceProvider().CreateScope().ServiceProvider;
+        _mediator = _provider.GetRequiredService<IMediator>();
+    }
+
     [Fact]
     public async Task Should_CreateOrder_When_OrderIsCorrect()
     {
         // Arrange
-        var services = new ServiceCollection();
-        services.AddDbContext<DatabaseContext>();
-        services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(Program).Assembly));
-        var provider = services.BuildServiceProvider().CreateScope().ServiceProvider;
-        var mediator = provider.GetRequiredService<IMediator>();
-        var orderToCreate = new Order
+        var order = new Order
         {
             Customer = "test",
             OrderNumber = "123"
         };
 
         // Act
-        var result = await mediator.Send(new CreateOrderCommand(orderToCreate));
+        var result = await _mediator.Send(new CreateOrderCommand(order));
 
         // Assert
         Assert.NotNull(result);
         Assert.True(result.Id > 0);
-        Assert.Equal(orderToCreate.Customer, result.Customer);
-        Assert.Equal(orderToCreate.OrderNumber, result.OrderNumber);
+        Assert.Equal(order.Customer, result.Customer);
+        Assert.Equal(order.OrderNumber, result.OrderNumber);
+    }
+
+    [Fact]
+    public async Task Should_UpdateOrder_When_OrderIsCorrect()
+    {
+        // Arrange
+        var order = _provider
+            .GetRequiredService<DatabaseContext>()
+            .Orders
+            .AsNoTracking()
+            .OrderByDescending(x => x.Id).FirstOrDefault();
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public async Task Should_DeleteOrder_When_OrderExists()
+    {
+        // Arrange
+        var order = _provider
+            .GetRequiredService<DatabaseContext>()
+            .Orders
+            .AsNoTracking()
+            .OrderByDescending(x => x.Id)
+            .FirstOrDefault();
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public async Task Should_NotThrowError_When_DeletingNonExistingOrder()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+    }
+
+    [Fact]
+    public async Task Should_ThrowError_When_UpdatingNonExistingOrder()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
     }
 }
